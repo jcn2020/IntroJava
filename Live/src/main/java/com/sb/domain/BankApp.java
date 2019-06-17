bd740a38dbef7c483292d48ebdf866614755a5e4
thethirdlane <anil@panix.com>
2019-06-17 16:43:06 -0400

    commit

--- Live/src/main/java/com/sb/domain/BankApp.java
+++ Live/src/main/java/com/sb/domain/BankApp.java
@@ -6,6 +6,15 @@ public class BankApp {
 		Account a1 = new Account(2, "Joe", 33333.00);
 		Account a2 = new Account(3, "Charlene", 38383.93);
 		
+	
+		Account [] arr = new Account[10];
+		
+		Account [] ar2 = { new Account(), new Account() };
+		
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = new Account();
+		}
+		
 		System.out.println("a1.name is " + a1.getName());
 		
 		
